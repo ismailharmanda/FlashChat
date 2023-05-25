484595c404f2c031557d8fa34432ebf963eac9ed
İsmail Harmanda <iharmanda@hotmail.com>
2023-05-26 02:47:39 +0300

    create MessageCell xib file

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by ismail harmanda on 26.05.2023.
+//  Copyright © 2023 ismail harmanda. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
