879d5d771870667467003f9c03a81a8637dbb38c
İsmail Harmanda <iharmanda@hotmail.com>
2023-05-25 19:49:34 +0300

    create UITableView and a Message Model

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by ismail harmanda on 25.05.2023.
+//  Copyright © 2023 ismail harmanda. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    let sender : String
+    let body : String
+}
