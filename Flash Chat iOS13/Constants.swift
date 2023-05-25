4dfaa1f0fa45e27a7d825e440244b1dba7da15f2
İsmail Harmanda <iharmanda@hotmail.com>
2023-05-25 18:00:43 +0300

    use a constants file in order to define segues

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by ismail harmanda on 25.05.2023.
+//
+
+struct Constants {
+    
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+}
--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -24,7 +24,7 @@ class LoginViewController: UIViewController {
                     print(e)
                 } else {
                     // Navigate to the ChatViewController
-                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                    self.performSegue(withIdentifier: Constants.loginSegue, sender: self)
                     
                 }
             }
